be962ca7f01e86e70557db32d4d164561be30901
Raivo Nikolajenko <raivuc99@gmail.com>
2021-04-27 14:30:13 +0300

    Salaboju JS,HTML,CSS lietas

--- index.js
+++ index.js
@@ -28,17 +28,4 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
 }
